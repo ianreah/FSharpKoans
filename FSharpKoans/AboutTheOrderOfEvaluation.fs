d3ef77fd44903d6e3888c871e01e6785511a9c0b
Ian Reah <ian.reah@nonlinear.com>
2014-04-04 10:51:43 +0100

    Making the assertions in AboutTheOrderOfEvaluation.fs pass
    What did I learn?...
    
    + Parentheses are used to group  things when you need to be explicit about the order of execution
    + Be careful about using parentheses where you don't need them (e.g., when calling a function)...
    
    	add (1 2)
    
       This will give a compiler error "This value is not a function and cannot be applied".  I think, because
       it tries to evaluate '1' as a function with '2' as its parameter, before passing the result into the 'add'
       function.
    
    + The backward pipe operator, <|, is another way of being explicit about order of execution.
    + The choice between using parentheses is really one of style and formatting (sometimes the backward
        pipe operator can make it more readable)
    + I need more practice interpreting F# compiler errors!

--- FSharpKoans/AboutTheOrderOfEvaluation.fs
+++ FSharpKoans/AboutTheOrderOfEvaluation.fs
@@ -17,10 +17,18 @@ module ``about the order of evaluation`` =
             x + y
 
         let result = add (add 5 8) (add 1 1)
+        //let result = add <| add 5 8 <| add 1 1
 
-        AssertEquality result __
+        AssertEquality result 15
 
         (* TRY IT: What happens if you remove the parensthesis?*)
+        (* You get 3 compiler errors...
+        Error	1	The type 'int -> 'a -> int -> int -> 'b' does not match the type 'int'	Line 17	Column 15
+        Error	2	Type mismatch. Expecting a 'a but given a 'a -> 'b -> 'c -> 'd -> 'e -> 'f -> 'g 
+                    The resulting type would be infinite when unifying ''a' and ''a -> 'b -> 'c -> 'd -> 'e -> 'f -> 'g' Line 19 Column 26
+        Error	3	Type mismatch. Expecting a 'a but given a 'b -> int -> int -> 'a -> 'c -> 'd -> 'e
+                    The resulting type would be infinite when unifying ''a' and ''b -> int -> int -> 'a -> 'c -> 'd -> 'e' Line 19 Column 34
+        I obviously need some practice interpreting F# compiler errors! *)
 
     [<Koan>]
     let TheBackwardPipeOperatorCanAlsoHelpWithGrouping() =
@@ -30,6 +38,7 @@ module ``about the order of evaluation`` =
         let double x =
             x * 2
 
-        let result = double <| add 5 8
+        //let result = double <| add 5 8
+        let result = double (add 5 8)
 
-        AssertEquality result __
+        AssertEquality result 26
